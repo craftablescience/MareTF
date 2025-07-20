c726e05e5103c52a6a63d054857e5d3305629c78
craftablescience <me@craftablescience.info>
2025-07-20 07:59:23 -0700

    gui: fix windows build again

--- install/_install.cmake
+++ install/_install.cmake
@@ -14,10 +14,15 @@ if(WIN32)
                 LIBRARY DESTINATION .)
 
         install(FILES "${QT_BASEDIR}/plugins/platforms/qwindows${QT_LIB_SUFFIX}.dll"
-                DESTINATION platforms)
+                DESTINATION "platforms")
 
-        install(FILES "${QT_BASEDIR}/plugins/styles/qwindowsvistastyle${QT_LIB_SUFFIX}.dll"
-                DESTINATION styles)
+        if(EXISTS "${QT_BASEDIR}/plugins/styles/qmodernwindowsstyle${QT_LIB_SUFFIX}.dll")
+            install(FILES "${QT_BASEDIR}/plugins/styles/qmodernwindowsstyle${QT_LIB_SUFFIX}.dll"
+                    DESTINATION "styles")
+        else()
+            install(FILES "${QT_BASEDIR}/plugins/styles/qwindowsvistastyle${QT_LIB_SUFFIX}.dll"
+                    DESTINATION "styles")
+        endif()
 
         # NSIS install commands
         configure_file(
