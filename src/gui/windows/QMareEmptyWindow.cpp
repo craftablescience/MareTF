8fbc72e84587c55727e17f9e26ffde879ed3e5a1
craftablescience <me@craftablescience.info>
2026-04-03 03:58:51 -0700

    Revert "gui: username compressed the splash art by 2.3mb, magic"
    This reverts commit 8ac0f40ea3bec3402d005dd937638bac36619cd8.

--- src/gui/windows/QMareEmptyWindow.cpp
+++ src/gui/windows/QMareEmptyWindow.cpp
@@ -94,7 +94,7 @@ QMareEmptyWindow::QMareEmptyWindow() : QMainWindow{nullptr} {
 void QMareEmptyWindow::paintEvent(QPaintEvent*) {
 	QPainter painter{this};
 
-	static const QPixmap SPLASH_IMAGE{":/splash.jpg"};
+	static const QPixmap SPLASH_IMAGE{":/splash.png"};
 	const QPixmap splashImageScaled = SPLASH_IMAGE.scaled(this->width(), this->height() - this->toolbar->height(), Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
 	painter.drawPixmap(0, this->toolbar->height(), splashImageScaled.width(), splashImageScaled.height(), splashImageScaled);
 }
