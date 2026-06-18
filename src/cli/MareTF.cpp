bbe5bb9efad513dcfcc4a9d645023f30b6827cda
craftablescience <me@craftablescience.info>
2026-06-17 19:06:13 -0700

    all: use maretf website for homepage url

--- src/cli/MareTF.cpp
+++ src/cli/MareTF.cpp
@@ -1456,7 +1456,7 @@ std::tuple<int, std::string> maretf_cli(int argc, const char* const argv[], QWid
 		" • maretf edit input.360.vtf -o input.vtf --set-platform PC --set-version 7.6 --recompute-mips\n"
 		" • maretf info input.vtf\n"
 		"See the project README for more information.\n\n"
-		"Want to report a bug or request a feature? Make an issue at " PROJECT_HOMEPAGE_URL "/issues"
+		"Want to report a bug or request a feature? Make an issue at " PROJECT_GITHUB_URL "/issues"
 	};
 
 	cli.add_epilog(enumInfo + std::string{PROGRAM_DETAILS});
